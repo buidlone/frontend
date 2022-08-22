e51652ed8b22ea29b86f41177032b961480ab033
Razmarataitë Gabeta <gabeta.razmarataite@mif.stud.vu.lt>
2022-08-22 18:11:43 +0300

    Dynamic rendering of progress steps; Tooltip

--- src/context/projectContext.tsx
+++ src/context/projectContext.tsx
@@ -1,47 +1,14 @@
 import { createContext, useState } from 'react';
+import project from './data';
 
-interface props {
+interface Props {
   children: React.ReactNode;
 }
 
-interface IFeaturedProject {
-  raised?: number;
-  milestones?: number;
-  milestonesCompleted?: number;
-  participants?: number;
-  funds?: number;
-  fundsReleased?: number;
-  tokensReserved?: number;
-  end?: string;
+const ProjectContext = createContext(project);
 
-  project?: string;
-  invested?: number;
-  collected?: number;
-  claimed?: number;
-  reserved?: number;
-  state?: string;
-}
-
-const ProjectContext = createContext<IFeaturedProject | undefined>(undefined);
-
-export function ProjectContextProdvider({ children }: props) {
-  const [featuredProject, setFeaturedProject] = useState<IFeaturedProject>({
-    raised: 1245,
-    milestones: 32,
-    milestonesCompleted: 12,
-    participants: 234,
-    funds: 324234,
-    fundsReleased: 23452,
-    tokensReserved: 4000000,
-    end: '2022-09-09',
-
-    project: 'Buidl1',
-    invested: 1245,
-    collected: 125,
-    claimed: 65,
-    reserved: 6585,
-    state: 'Ongoing',
-  });
+export function ProjectContextProdvider({ children }: Props) {
+  const [featuredProject, setFeaturedProject] = useState(project);
 
   return (
     <ProjectContext.Provider value={featuredProject}>
