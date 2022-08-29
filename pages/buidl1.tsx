a7961b8a6cd2f6d14ee5658c21f1c600d7614c66
gabyteera <gabeta.razmarataite@gmail.com>
2022-08-29 16:39:01 +0300

    Footer component

--- pages/buidl1.tsx
+++ pages/buidl1.tsx
@@ -1,10 +1,11 @@
-import AboutSection from '../src/components/aboutSection';
-import ProgressSection from '../src/components/progressSection';
-import { FeaturesSec } from '../src/components/featuredProjectsSection/styled';
-import FundingBlock from '../src/components/fundingBlock';
-import ProjectHeader from '../src/components/projectHeader';
-import { BgImage, Container } from '../styles/Container';
-import TimelineBlock from '../src/components/timelineBlock';
+import AboutSection from "../src/components/aboutSection";
+import ProgressSection from "../src/components/progressSection";
+import { FeaturesSec } from "../src/components/featuredProjectsSection/styled";
+import FundingBlock from "../src/components/fundingBlock";
+import ProjectHeader from "../src/components/projectHeader";
+import { BgImage, Container } from "../styles/Container";
+import TimelineBlock from "../src/components/timelineBlock";
+import FooterSection from "../src/components/footerSection";
 
 const Buidl1 = () => {
   return (
@@ -12,8 +13,8 @@ const Buidl1 = () => {
       <BgImage isFixed />
       <Container>
         <ProjectHeader
-          text={'Project 1 - BUIDL 1 - Self raising capital'}
-          stage={'STAGE 1'}
+          text={"Project 1 - BUIDL 1 - Self raising capital"}
+          stage={"STAGE 1"}
         />
         <FeaturesSec>
           <FundingBlock />
@@ -22,6 +23,8 @@ const Buidl1 = () => {
         <ProgressSection />
         <AboutSection />
       </Container>
+
+      <FooterSection />
     </>
   );
 };
---
+++ src/components/footerSection/styled.tsx
@@ -0,0 +1,118 @@
+import Image from "next/image";
+import styled, { css } from "styled-components";
+
+export const FooterSectionWrapper = styled.div`
+  width: 100%;
+  height: 33.75rem;
+  background: #132744;
+  padding-bottom: 6rem;
+  display: flex;
+  padding-left: 6rem;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+  flex-wrap: wrap;
+
+  & > * {
+    flex-shrink: 1;
+  }
+
+  @media screen and (max-width: 1190px) {
+    padding-left: 0;
+  }
+`;
+
+export const FooterHeader = styled.p`
+  font-family: "Roboto" sans-serif;
+  font-size: 40px;
+  color: #ffffff;
+  opacity: 1;
+  position: relative;
+  margin: 1rem auto;
+
+  &:before {
+    content: "";
+    background: #ffeb00 0% 0% no-repeat padding-box;
+    box-shadow: 0px 0px 10px #ffed89;
+    opacity: 1;
+    right: calc(100% + 1.3rem);
+    top: calc(100% - 1.7rem);
+    border-radius: 50%;
+    width: 11px;
+    height: 11px;
+    position: absolute;
+  }
+`;
+
+export const MainText = styled.div`
+  text-align: center;
+  font-size: 14px;
+  font-family: Arial, Helvetica, sans-serif "Arial";
+  display: inline-block;
+  max-width: 41.188rem;
+  line-height: 25px;
+  white-space: normal;
+  color: #ffffff;
+`;
+
+export const LogoWrapper = styled.div`
+  display: flex;
+  align-items: space-evenly;
+  gap: 1rem;
+  padding-right: 6rem;
+
+  & > a {
+    height: 1.5rem;
+    width: 1.5rem;
+  }
+  & > a:first-child {
+    margin-right: 0.5rem;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: space-between;
+  justify-content: center;
+  gap: 1.2rem;
+`;
+
+export const InputLine1 = styled.div`
+  background-color: #c6e5fe;
+  width: 28.063rem;
+  height: 2.188rem;
+`;
+
+export const InputLine2 = styled.div`
+  background-color: #c6e5fe;
+  width: 9.188rem;
+  height: 2.188rem;
+  margin-right: 13rem;
+`;
+
+export const ContactsLine = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  margin-top: 2rem;
+  width: 100%;
+
+  @media screen and (max-width: 1190px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+
+    ${InputLine2} {
+      margin-right: 0rem;
+    }
+
+    & > ${LogoWrapper} {
+      order: 2;
+      padding-right: 0;
+    }
+  }
+`;
