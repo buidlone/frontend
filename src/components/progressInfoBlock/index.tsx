8c8f1e166b1e0ad5a3601d838cbff45a95869cac
gabyteera <gabeta.razmarataite@gmail.com>
2022-09-28 16:15:19 +0300

    reading from SC and displaying voting tokens

--- src/components/progressInfoBlock/index.tsx
+++ src/components/progressInfoBlock/index.tsx
@@ -1,4 +1,4 @@
-import { Property, Data } from '../detailsBlock/styled';
+import { Property, Data } from "../detailsBlock/styled";
 import {
   BottomPartWrapper,
   BottomWrapper,
@@ -8,20 +8,40 @@ import {
   InfoIconKeys,
   KeysWrapper,
   OrangeButton,
-} from './styled';
-import Image from 'next/image';
-import DiscordImg from '../../../public/DiscordSmall.png';
-import OrangeLock from '../../../public/OrangeLock.svg';
-import { TableLink } from '../activeBlock/styled';
-import { useContext } from 'react';
-import ProjectContext from '../../context/projectContext';
-import useCountdown from '../../hooks/useCountdown';
-import Tooltip from '../tooltip';
+} from "./styled";
+import Image from "next/image";
+import DiscordImg from "../../../public/DiscordSmall.png";
+import OrangeLock from "../../../public/OrangeLock.svg";
+import { TableLink } from "../activeBlock/styled";
+import { useContext, useEffect, useState } from "react";
+import ProjectContext from "../../context/projectContext";
+import useCountdown from "../../hooks/useCountdown";
+import Tooltip from "../tooltip";
+import { Web3Provider } from "@ethersproject/providers";
+import { getVotingTokens } from "../../web3/getVotingTokens";
+import Web3Context from "../../context/web3Context";
 
 const ProgressInfoBlock = () => {
   const featuredProject = useContext(ProjectContext);
   const { timerDays, timerHours, timerMinutes, timerSeconds, isExpired } =
     useCountdown(featuredProject?.end);
+
+  const { web3Provider, address } = useContext(Web3Context);
+  const [votingTokensSupply, setVotingTokensSupply] = useState<
+    String | undefined
+  >(undefined);
+  const [votingTokenBalance, setVotingTokenBalance] = useState<
+    String | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (Web3Provider) {
+      getVotingTokens(web3Provider, address).then((data) => {
+        setVotingTokensSupply(data?.votingTokensSupply);
+        setVotingTokenBalance(data?.votingTokenBalance);
+      });
+    }
+  }, [web3Provider]);
 
   return (
     <DetailsCard>
@@ -44,12 +64,12 @@ const ProgressInfoBlock = () => {
         <Data>{featuredProject?.participants} wallets</Data>
 
         <Data>
-          {featuredProject?.fundsReleased?.toLocaleString().replace(/,/g, ' ')}{' '}
-          / {featuredProject?.funds?.toLocaleString().replace(/,/g, ' ')} USDT
+          {featuredProject?.fundsReleased?.toLocaleString().replace(/,/g, " ")}{" "}
+          / {featuredProject?.funds?.toLocaleString().replace(/,/g, " ")} USDT
         </Data>
 
         <Data>
-          {featuredProject?.tokensReserved?.toLocaleString().replace(/,/g, ' ')}{' '}
+          {featuredProject?.tokensReserved?.toLocaleString().replace(/,/g, " ")}{" "}
           DPP
         </Data>
 
@@ -63,28 +83,37 @@ const ProgressInfoBlock = () => {
           <KeysWrapper>
             <Image
               src={OrangeLock}
-              alt={'Locked lock'}
-              height={'26px'}
-              width={'26px'}
+              alt={"Locked lock"}
+              height={"26px"}
+              width={"26px"}
             />
-            <div>Keys activated</div>
-            <div> 0 / 1223</div>
 
-            <Tooltip text={'Information about the keys'}>
-              <InfoIconKeys />
-            </Tooltip>
+            {web3Provider && votingTokenBalance && votingTokensSupply ? (
+              <>
+                <div>Keys activated</div>
+                <div>
+                  {" "}
+                  {votingTokenBalance} / {votingTokensSupply}
+                </div>
+                <Tooltip text={"Information about the keys"}>
+                  <InfoIconKeys />
+                </Tooltip>
+              </>
+            ) : (
+              <div>Connect Your wallet to view the keys</div>
+            )}
           </KeysWrapper>
           <KeysWrapper>
             <Image
               src={DiscordImg}
-              alt={'Discord logo'}
-              height={'26px'}
-              width={'26px'}
+              alt={"Discord logo"}
+              height={"26px"}
+              width={"26px"}
             />
             <TableLink>Project discussion</TableLink>
           </KeysWrapper>
         </BottomPartWrapper>
-        <BottomPartWrapper className='centerItems'>
+        <BottomPartWrapper className="centerItems">
           <OrangeButton>STOP</OrangeButton>
           <TableLink>Trust us? Try burning the ticket</TableLink>
         </BottomPartWrapper>
