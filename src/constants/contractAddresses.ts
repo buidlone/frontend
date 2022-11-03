5e12bc533232fad2211ac80a9133e987618560ea
GabijaBeres <beresneviciute.gabija@gmail.com>
2022-11-03 09:34:35 +0200

    stop project finished

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -1,5 +1,5 @@
 export const GovernancePoolAddress = "0x7FEA3A43A2E31b4804c12ADDCac1E70f851Ccc91";
-export const InvestmentPoolAddress = "0x19e0b0777a04b72afF5aD06a5e094E1725369026";
+export const InvestmentPoolAddress = "0x4C34Adb12AC0E1A6D8c64EE00e478072Aa7d4Ab2";
 export const InvestmentPoolFactoryAddress = "0x1d13caAf1F1C56A008e5A0bFC55ACa89376Eaa25";
 export const VotingTokenAddress = "0x73B004A90601751Ab29ace916F54AB9284E7B49C";
 
--- src/web3/stopProject.ts
+++ src/web3/stopProject.ts
@@ -14,55 +14,19 @@ export const stopProject = async (provider: any, address: string | undefined | n
         const signer = provider.getSigner();
         const contractGovernancePoolProvider = new ethers.Contract(GovernancePoolAddress, GovernancePoolABI, provider);
         const contractVotingToken = new ethers.Contract(VotingTokenAddress, VotingTokenABI, provider);
-        const contractInvestmentPool = new ethers.Contract(InvestmentPoolAddress, InvestmentPoolABI, provider)
-        const contractInvestmentPoolSigner = new ethers.Contract(InvestmentPoolAddress, InvestmentPoolABI, signer)
         const contractGovernancePoolSigner = new ethers.Contract(GovernancePoolAddress, GovernancePoolABI, signer)
         const contractVotingTokenSigner = new ethers.Contract(VotingTokenAddress, VotingTokenABI, signer);
-
         const votingTokenBalance = await contractGovernancePoolProvider.getVotingTokenBalance(InvestmentPoolAddress, address);   
-        // const isApprovedPromise = contractVotingToken.isApprovedForAll(address, GovernancePoolAddress)
-        // let isApproved = await isApprovedPromise;
+        const isApprovedPromise = contractVotingToken.isApprovedForAll(address, GovernancePoolAddress)
+        let isApproved = await isApprovedPromise;
 
-        // if(!isApproved) {
-        //   isApproved = await contractVotingTokenSigner.setApprovalForAll(GovernancePoolAddress, true)
-        // }
-        //  if(isApproved) {
-        //    stop = await contractGovernancePoolSigner.voteAgainst(InvestmentPoolAddress, votingTokenBalance)
-        //  }
+        if(!isApproved) {
+          isApproved = await contractVotingTokenSigner.setApprovalForAll(GovernancePoolAddress, true)
+        }
+         if(isApproved) {
+           stop = await contractGovernancePoolSigner.voteAgainst(InvestmentPoolAddress, votingTokenBalance)
+         }
 
-
-
- 
-
-// const date = await contractInvestmentPool.milestones(0);
- //const timestamp = await contractInvestmentPoolSigner.setTimestamp(1666621151)
-//  const state = await  contractInvestmentPool.getProjectStateByteValue();
-
-// console.log(state);
-// console.log("state");
-
-// console.log(timestamp)
-// console.log("timestamp");
-
-// console.log(date)
-// console.log("date");
-
-
-      
-
-
-       
-
-       
-         
-         
-         
-
-  console.log(stop);
-  
-      return {
-    
-      }
     } catch (error) {
       console.log(error);
       toast.error("Error occurred while retrieving data from blockchain")
