b9b46af34913410d76398799ce6f6b7ac349695f
GabijaBeres <beresneviciute.gabija@gmail.com>
2022-11-04 14:36:59 +0200

    errors in stop function

--- src/web3/stopProject.ts
+++ src/web3/stopProject.ts
@@ -4,6 +4,8 @@ import { GovernancePoolAddress, InvestmentPoolAddress, VotingTokenAddress } from
 import GovernancePoolABI from './abi/GovernancePool.json'
 import VotingTokenABI from './abi/VotingToken.json'
 import InvestmentPoolABI from './abi/InvestmentPool.json'
+import { log } from "console";
+import { VoteAgainstErrorEnum } from "../interfaces/enums/VoteAgainstErrorEnums";
 
 
 export const stopProject = async (provider: any, address: string | undefined | null) => {
@@ -22,7 +24,7 @@ export const stopProject = async (provider: any, address: string | undefined | n
         const currentMilestone = await contractInvestmentPoolProvider.getCurrentMilestoneId();
         const activeTokens = await contractGovernancePoolProvider.getActiveVotingTokensBalance(InvestmentPoolAddress, currentMilestone, address)
         const usedTokens = await contractGovernancePoolProvider.getVotesAmount(address, investmentPoolId)
-        const votesBalance = (activeTokens - usedTokens)/5;
+        const votesBalance = (activeTokens - usedTokens);
         const isApprovedPromise = contractVotingToken.isApprovedForAll(address, GovernancePoolAddress)
         let isApproved = await isApprovedPromise;
 
@@ -36,11 +38,27 @@ export const stopProject = async (provider: any, address: string | undefined | n
 
         }
          if(isApproved) {
-          const voteTx = await contractGovernancePoolSigner.voteAgainst(
-            InvestmentPoolAddress,
-            votesBalance
-        );
-        const voteReceipt = await voteTx.wait();
+
+          try {
+            const voteTx = await contractGovernancePoolSigner.voteAgainst(
+              InvestmentPoolAddress,
+              votesBalance
+          );
+          const voteReceipt = await voteTx.wait();
+            } catch (err: any) {
+                const revertData = err.error.data.originalError.data;
+                const decodedError =  contractGovernancePoolSigner.interface.parseError(revertData);
+
+                if(decodedError.name === VoteAgainstErrorEnum.AMOUNT_ZERO) {
+                  toast.error("Voting token amount must be greater than zero")
+                } else if (decodedError.name === VoteAgainstErrorEnum.NO_TOKENS) {
+                  toast.error("No active voting tokens")
+                } else if (decodedError.name === VoteAgainstErrorEnum.AMOUNT_IS_GREATER_THAN_BALANCE) {
+                  toast.error("Amount is greater than voting tokens balance")
+                } else if (decodedError.name === VoteAgainstErrorEnum.BAD_STATE) {
+                  toast.error("Project state is not suitable for stopping the project")
+                }
+            }
          }
        
 
