2e213020c48d9edc62a48f75ef99859506be076d
GabijaBeres <beresneviciute.gabija@gmail.com>
2022-11-08 13:47:01 +0200

    history table

--- src/components/historyBlock/index.tsx
+++ src/components/historyBlock/index.tsx
@@ -1,20 +1,79 @@
-import { useContext, useEffect } from "react";
+import React from "react";
+import { useContext, useEffect, useState } from "react";
+import LoadedValuesContext from "../../context/loadedValuesContext";
 import Web3Context from "../../context/web3Context";
 import { getHistoryTable } from "../../web3/historyTable";
+import { BarChartContainer, BarChartScroll } from "../investorsBarChart/styled";
+import { Table } from "../tokenStreamTable/styled";
 import { HistoryTable } from "./styled";
 
 const HistoryBlock = () => {
+  type History = {
+    address: string;
+    hash: string;
+    amount: string;
+  };
   const { web3Provider, connect, address } = useContext(Web3Context);
+  const [history, setHistory] = useState<History[]>();
+  const { currency } = useContext(LoadedValuesContext);
+
   useEffect(() => {
     if (web3Provider) {
-      getHistoryTable(web3Provider, address).then((data: any) => {
-        console.log(data);
+      getHistoryTable(web3Provider).then((data: any) => {
+        setHistory(data);
       });
-    } else {
     }
-  });
+  }, []);
 
-  return <HistoryTable style={{ color: "white" }}>History Table</HistoryTable>;
+  if (history) {
+    return (
+      <HistoryTable style={{ color: "white" }}>
+        <BarChartContainer>
+          <BarChartScroll hideScrollbars={false} vertical={true}>
+            <Table>
+              <thead>
+                <th>Address</th>
+                <th>Amount</th>
+                <th>Transaction Hash</th>
+              </thead>
+              <tbody>
+                {history.map((item: any) => (
+                  <tr>
+                    <td data-label={`Address`} className="token">
+                      {item.address}
+                    </td>
+                    <td data-label={`Amount`} className="token">
+                      {item.amount} {currency.label}
+                    </td>
+                    <td data-label={`Transaction Hash`} className="fund">
+                      <a
+                        target="_blank"
+                        href={`https://goerli.etherscan.io/tx/${item.hash}`}
+                      >
+                        {item.hash}
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </BarChartScroll>
+        </BarChartContainer>
+      </HistoryTable>
+    );
+  }
+  return (
+    <HistoryTable style={{ color: "white" }}>
+      <Table>
+        <thead>
+          <th>Address</th>
+          <th>Amount</th>
+          <th>Transaction Hash</th>
+        </thead>
+        <tbody></tbody>
+      </Table>
+    </HistoryTable>
+  );
 };
 
 export default HistoryBlock;
--- src/web3/historyTable.ts
+++ src/web3/historyTable.ts
@@ -1,37 +1,36 @@
 import { BigNumber, ethers } from "ethers";
 import { toast } from "react-toastify";
 import { InvestmentPoolAddress } from "../constants/contractAddresses";
-import InvestmentPoolABI from './abi/InvestmentPool.json'
+import InvestmentPoolABI from "./abi/InvestmentPool.json";
 
-export const getHistoryTable = async (provider: any, address?: null | string) => {
+export const getHistoryTable = async (provider: any) => {
+  try {
+    const contract = new ethers.Contract(
+      InvestmentPoolAddress,
+      InvestmentPoolABI,
+      provider
+    );
+    const filter = contract.filters.Invest();
+    const filteredEvents = await contract.queryFilter(filter);
+    let history = [{}];
 
-  let totalAmountInvestedBN = BigNumber.from(0)
+    for (let i = 0; i < filteredEvents.length; i++) {
+      const address = filteredEvents[i].args?.caller;
+      const hash = filteredEvents[i].transactionHash;
+      const amount = ethers.utils.formatEther(filteredEvents[i].args?.amount);
 
-     try {
-        const contract = new ethers.Contract(InvestmentPoolAddress, InvestmentPoolABI, provider);
-        const filter = contract.filters.Invest(address);
-        const filteredEvents = await contract.queryFilter(filter);
-       
-        for (let i in filteredEvents) {
-          
-          const investedAmount = filteredEvents[i].args?.amount
-          totalAmountInvestedBN = totalAmountInvestedBN.add(investedAmount);
-
-          console.log(investedAmount)
-          console.log(totalAmountInvestedBN)
-
-        }
-        const totalAmountInvested = ethers.utils.formatEther(totalAmountInvestedBN)
-
-        
-      return {
-        totalAmountInvested
-      }
-
-    } catch (error) {
-      console.log(error);
-      toast.error("Error occurred while retrieving data from blockchain")
+      history.splice(Number(i), 0, {
+        address: address,
+        amount: amount,
+        hash: hash,
+      });
     }
-  
+    history.pop();
+    history = [...history].reverse();
 
+    return history;
+  } catch (error) {
+    console.log(error);
+    toast.error("Error occurred while retrieving data from blockchain");
+  }
 };
