de137f2c95f060f69f98ed8c3c873af566748cae
GabijaBeres <beresneviciute.gabija@gmail.com>
2022-12-12 07:39:45 +0200

    ido details

--- src/components/detailsBlock/index.tsx
+++ src/components/detailsBlock/index.tsx
@@ -1,7 +1,6 @@
 import { ethers } from "ethers";
 import { useContext } from "react";
 import LoadedValuesContext from "../../context/loadedValuesContext";
-import ProjectContext from "../../context/projectContext";
 import useCountdown from "../../hooks/useCountdown";
 import { IInvestorsProps } from "../../interfaces/ICommonProps";
 import {
@@ -13,39 +12,27 @@ import {
   DetailsContentWrapper,
 } from "./styled";
 
-export const featuredProject = {
-  raised: "1245 ETH",
-  milestones: "12/32",
-  participants: "234 wallets",
-  funds: "23452/324234 USDT",
-  tokens: "4 000 000 DPP",
-  end: "23D 03H 30M 23S",
-};
-
 const DetailsBlock = ({ wallets, ...props }: IInvestorsProps) => {
-  const featuredProject = useContext(ProjectContext);
-  const { totalInvested, currency, milestones, currentMilestone } =
+  const { currency, milestones, currentMilestone, hardCap } =
     useContext(LoadedValuesContext);
-  const { timerDays, timerHours, timerMinutes, timerSeconds, isExpired } =
-    useCountdown(milestones[milestones.length - 1].endDate);
+  const { timerDays, timerHours, timerMinutes, timerSeconds } = useCountdown(
+    milestones[milestones.length - 1].endDate
+  );
 
   return (
     <DetailsBlockWrapper>
       <DetailsContentWrapper>
         <FlexItem>
           <FlexItem1>
-            <Property className="bigger">Raised</Property>
+            <Property className="bigger">Round</Property>
             <Property className="medium">Milestones completed</Property>
             <Property className="medium">Participants</Property>
-            <Property>Funds released</Property>
-            <Property>Tokens reserved</Property>
+            <Property>Hard cap</Property>
+            <Property>Project start</Property>
             <Property>Project ends in</Property>
           </FlexItem1>
           <FlexItem1>
-            <Data>
-              {ethers.utils.formatEther(totalInvested)} {currency.label}
-            </Data>
-
+            <Data>Seed</Data>
             <Data>
               {currentMilestone}/{milestones.length}
             </Data>
@@ -57,22 +44,13 @@ const DetailsBlock = ({ wallets, ...props }: IInvestorsProps) => {
                 : "wallets"}
             </Data>
 
-            <Data className="medium">
-              {featuredProject?.fundsReleased
-                ?.toLocaleString()
-                .replace(/,/g, " ")}{" "}
-              / {featuredProject?.funds?.toLocaleString().replace(/,/g, " ")}{" "}
-              {currency.label}
+            <Data>
+              {ethers.utils.formatEther(hardCap)} {currency.label}
             </Data>
+
+            <Data className="medium">{milestones[0].startDate}</Data>
 
             <Data className="smaller">
-              {featuredProject?.tokensReserved
-                ?.toLocaleString()
-                .replace(/,/g, " ")}{" "}
-              DPP
-            </Data>
-
-            <Data className="blue smaller">
               {timerDays}D {timerHours}H {timerMinutes}M {timerSeconds}S
             </Data>
           </FlexItem1>
@@ -80,47 +58,25 @@ const DetailsBlock = ({ wallets, ...props }: IInvestorsProps) => {
 
         <FlexItem>
           <FlexItem1>
-            <Property className="bigger">Raised</Property>
-            <Property className="medium">Milestones completed</Property>
-            <Property className="medium">Participants</Property>
-            <Property>Funds released</Property>
-            <Property>Tokens reserved</Property>
-            <Property>Project ends in</Property>
+            <Property className="bigger">Token name</Property>
+            <Property className="medium">Ticker</Property>
+            <Property>Network</Property>
+            <Property>Initial token supply</Property>
+            <Property>Total supply</Property>
+            <Property>Token price</Property>
           </FlexItem1>
           <FlexItem1>
-            <Data>
-              {ethers.utils.formatEther(totalInvested)} {currency.label}
-            </Data>
+            <Data>BUIDL1</Data>
 
-            <Data>
-              {currentMilestone}/{milestones.length}
-            </Data>
+            <Data>BDL1</Data>
 
-            <Data>
-              {wallets[0] !== "" ? wallets?.length : 0}{" "}
-              {wallets?.length === 1 && wallets[0] !== ""
-                ? "wallet"
-                : "wallets"}
-            </Data>
+            <Data>Goerli Testnet</Data>
 
-            <Data className="medium">
-              {featuredProject?.fundsReleased
-                ?.toLocaleString()
-                .replace(/,/g, " ")}{" "}
-              / {featuredProject?.funds?.toLocaleString().replace(/,/g, " ")}{" "}
-              {currency.label}
-            </Data>
+            <Data className="medium">..</Data>
 
-            <Data className="smaller">
-              {featuredProject?.tokensReserved
-                ?.toLocaleString()
-                .replace(/,/g, " ")}{" "}
-              DPP
-            </Data>
+            <Data className="smaller">...</Data>
 
-            <Data className="blue smaller">
-              {timerDays}D {timerHours}H {timerMinutes}M {timerSeconds}S
-            </Data>
+            <Data className="smaller">-</Data>
           </FlexItem1>
         </FlexItem>
       </DetailsContentWrapper>
@@ -129,3 +85,4 @@ const DetailsBlock = ({ wallets, ...props }: IInvestorsProps) => {
 };
 
 export default DetailsBlock;
+
