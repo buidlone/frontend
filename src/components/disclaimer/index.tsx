677b830ae8edb89119f86c1b61fa5596e3183ebe
GabijaBeres <beresneviciute.gabija@gmail.com>
2022-12-16 15:56:10 +0200

    disclaimer message change

--- src/components/disclaimer/index.tsx
+++ src/components/disclaimer/index.tsx
@@ -1,13 +1,35 @@
 import { HideForDesktop, HideForMobile } from "../../../styles/Container";
-import { DisclaimerWrapper } from "./styled";
+import { DisclaimerWrapper, SocialsWrapper } from "./styled";
+import InstagramLogo from "../../../public/instagram_logo_white.svg";
+import TwitterLogo from "../../../public/twitter_logo_white.svg";
+import LinkedinLogo from "../../../public/linkedin_logo_white.svg";
+import DiscordLogo from "../../../public/discord_logo_white.svg";
+import Image from "next/image";
 
 const Disclaimer = ({ hideMobile }: any) => {
   if (hideMobile) {
     return (
       <HideForMobile>
         <DisclaimerWrapper>
-          &#9888; This page is not yet working for mainnet. This is just a
-          representation of future functionality for mainnet.
+          Test our protocol while we're preparing for mainnet. Keep updated:
+          <SocialsWrapper>
+            <a
+              href="https://discord.com/channels/998519974714941480/998519974714941483"
+              target="_blank"
+            >
+              <Image src={DiscordLogo} height="20px" />
+            </a>
+
+            <a href="https://www.linkedin.com/company/buidl1" target="_blank">
+              <Image src={LinkedinLogo} height="20px" />
+            </a>
+            <a href="https://twitter.com/buidlone" target="_blank">
+              <Image src={TwitterLogo} height="20px" />
+            </a>
+            <a href="https://www.instagram.com/buidl1/" target="_blank">
+              <Image src={InstagramLogo} height="20px" />
+            </a>
+          </SocialsWrapper>
         </DisclaimerWrapper>
       </HideForMobile>
     );
@@ -15,8 +37,25 @@ const Disclaimer = ({ hideMobile }: any) => {
     return (
       <HideForDesktop>
         <DisclaimerWrapper>
-          &#9888; This page is not yet working for mainnet. This is just a
-          representation of future functionality for mainnet.
+          Test our protocol while we're preparing for mainnet. Keep updated:
+          <SocialsWrapper>
+            <a
+              href="https://discord.com/channels/998519974714941480/998519974714941483"
+              target="_blank"
+            >
+              <Image src={DiscordLogo} height="20px" />
+            </a>
+
+            <a href="https://www.linkedin.com/company/buidl1" target="_blank">
+              <Image src={LinkedinLogo} height="20px" />
+            </a>
+            <a href="https://twitter.com/buidlone" target="_blank">
+              <Image src={TwitterLogo} height="20px" />
+            </a>
+            <a href="https://www.instagram.com/buidl1/" target="_blank">
+              <Image src={InstagramLogo} height="20px" />
+            </a>
+          </SocialsWrapper>
         </DisclaimerWrapper>
       </HideForDesktop>
     );
